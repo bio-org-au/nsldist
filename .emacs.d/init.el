cd4acf21fcd8ac5b16ef4e2519fbd37b0971221b
Chris <xpusostomos@gmail.com>
2026-01-08 11:10:28 +0800

    custom

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -31,38 +31,6 @@ There are two things you can do about this warning:
     (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
 (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
 (package-initialize)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#21252B" "#E06C75" "#98C379" "#E5C07B" "#61AFEF" "#C678DD" "#56B6C2"
-	"#ABB2BF"])
- '(custom-enabled-themes '(manoj-dark))
- '(custom-safe-themes
-   '("b23f3067e27a9940a563f1fb3bf455aabd0450cb02c3fa4ad43f75a583311216"
-	 "24fc62afe2e5f0609e436aa2427b396adf9a958a8fa660edbaab5fb13c08aae6"
-	 default))
- '(display-battery-mode t)
- '(display-time-mode t)
- '(menu-bar-mode nil)
- '(package-selected-packages
-   '(ace-jump-mode arc-dark-theme arjen-grey-theme async avy backlight
-				   buffer-move centaur-tabs company counsel counsel
-				   current-window-only dap-mode desktop-environment
-				   dir-treeview dir-treeview-themes dired-sidebar
-				   direx dirtree dirtree-prosjekt dmenu elgrep exwm
-				   exwm fancy-battery fancy-battery filetree
-				   find-things-fast fira-code-mode fontawesome
-				   grails-mode groovy-imports groovy-mode helm-lsp
-				   hide-comnt list-utils list-utils lsp-groovy lsp-ivy
-				   lsp-java lsp-javacomp lsp-mode lsp-ui magit
-				   mark-thing-at melpa-upstream-visit neotree
-				   projectile simple-httpd slime treesit-auto vterm
-				   winner-mode-enable xah-get-thing yaml-mode))
- '(tab-bar-mode t)
- '(tool-bar-mode nil))
 
 (setq-default buffer-file-coding-system 'utf-8-unix) ;; Unix line endings always
 
@@ -738,34 +706,8 @@ function."
 
 (setq project-vc-merge-submodules nil)
 (setq project-mode-line t)
+
+(setq custom-file (expand-file-name "custom-vars.el" user-emacs-directory))
  
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "FreeMono" :foundry "GNU " :slant normal :weight bold :height 143 :width normal))))
- '(font-lock-comment-delimiter-face ((t (:foreground "#808080"))))
- '(font-lock-comment-face ((t (:foreground "#808080" :slant oblique))))
- '(font-lock-constant-face ((t (:foreground "#6897bb" :weight bold))))
- '(font-lock-function-name-face ((t (:foreground "white" :weight normal))))
- '(font-lock-keyword-face ((t (:foreground "#CC7832"))))
- '(font-lock-string-face ((t (:foreground "#6a8759"))))
- '(font-lock-type-face ((t (:foreground "white"))))
- '(font-lock-variable-name-face ((t (:foreground "#9676aa"))))
- '(groovy-annotation-face ((t (:inherit font-lock-constant-face :foreground "#bbb429"))))
- '(minibuffer-prompt ((t (:background "tomato4" :foreground "white"))))
- '(mode-line ((t (:background "gray14" :foreground "white" :box (:line-width (1 . -1) :style released-button) :height 0.9))))
- '(mode-line-active ((t (:inherit mode-line :background "tomato4"))))
- '(mode-line-buffer-id ((t (:weight bold :height 0.9))))
- '(mode-line-inactive ((t (:background "gray14" :foreground "white" :box (:line-width (1 . 1) :color "gray40") :weight light :height 0.9))))
- '(nxml-attribute-local-name ((t (:inherit font-lock-variable-name-face :foreground "#bababa"))))
- '(nxml-element-local-name ((t (:inherit font-lock-function-name-face :foreground "#e8bf6a"))))
- '(rng-error ((t (:inherit font-lock-warning-face :foreground "#cc7832"))))
- '(tab-bar ((t (:inherit variable-pitch :background "black" :foreground "light gray"))))
- '(tab-line ((t (:inherit variable-pitch :background "gray14" :foreground "white" :box (:line-width (3 . 3) :color "gray14" :style released-button) :height 0.9))))
- '(tab-line-highlight ((t (:inherit nil :background "midnight blue" :foreground "WhiteSmoke" :box (:line-width (5 . 5) :color "black") :weight normal))))
- '(tab-line-tab ((t (:inherit nil :background "#440000" :foreground "WhiteSmoke" :box (:line-width (3 . 3) :color "pale goldenrod" :style released-button) :weight normal))))
- '(tab-line-tab-current ((t (:inherit nil :background "tomato4" :foreground "WhiteSmoke" :box (:line-width (3 . 3) :color "gold") :weight normal))))
- '(tab-line-buffer-id ((t (:inherit tab-line :background "blue"))))
- '(tab-line-tab-inactive ((t (:background "gray14" :foreground "white")))))
+(add-hook 'emacs-startup-hook
+          (lambda () (load custom-file 'noerror)))
