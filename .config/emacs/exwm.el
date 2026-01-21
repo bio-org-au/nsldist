350da32dbe56e33754646a69dc131ca8462b526f
Chris Bitmead <xpusostomos@gmail.com>
2026-01-21 10:16:30 +0800

    redshift

--- .config/emacs/exwm.el
+++ .config/emacs/exwm.el
@@ -528,6 +528,8 @@
   (efs/run-in-background "caffeine")
   (efs/run-in-background "pasystray");
   (efs/run-in-background "gxkb");
+  (efs/run-in-background "copyq");
+  (efs/run-in-background "redshift-gtk");
 ;;  (efs/run-in-background "jetbrains-toolbox")
   (efs/run-in-background "picom") ; composite manager, plank prefers it
 ;;  (efs/run-in-background "plank -n dock1")
